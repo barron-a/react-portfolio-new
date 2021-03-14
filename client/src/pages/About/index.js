80b22ebb81493dd163c288d8540e53f99faafb05
Adam Barron <adambarron@me.com>
2021-03-14 13:40:07 -0700

    centered content with classNames in About section

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -7,22 +7,26 @@ import Row from 'react-bootstrap/Row';
 export const About = () => {
     return (
         <Container fluid="md">
-            <Row className="justify-content-md-center">
-            <h1>About Me</h1>
+            <Row className="justify-content-center">
+                <h1>About Me</h1>
             </Row>
-            <Row className="justify-content-md-center">
-            <Figure>
-                <Figure.Image
-                    width={315}
-                    height={400}
-                    alt="Adam Barron Portrait"
-                    src={photo}
-                />
-                <Figure.Caption>
-                    Adam Barron Bio
-                </Figure.Caption>
-            </Figure>
+            <Row>
+                <Figure>
+                    <Figure.Image
+                        className="rounded mx-auto d-block"
+                        width={315}
+                        height={400}
+                        alt="Adam Barron Portrait"
+                        src={photo}
+                    />
+                    <Figure.Caption className="text-left">
+                        My name is Adam Barron, and I am currently working at Apple as a technician supervisor. My education and career have led me down a variety of paths including Industrial Design, Technology, and Management. In an effort to broaden my skills, I am now taking part in a full-stack coding bootcamp.
+                        <br />
+                        <br />
+                        In my free time, I enjoy listening to music, going to concerts and movies, spending time with family and friends, reading, learning about technology, and traveling whenever possible. I'm looking forward to new adventures in both my personal and professional life!
+                    </Figure.Caption>
+                </Figure>
             </Row>
         </Container>
-    ) 
+    )
 }
