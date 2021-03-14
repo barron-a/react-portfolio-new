818274b9d9cd52db9f20be2b3a7d01fb6560b5a0
Adam Barron <adambarron@me.com>
2021-03-14 12:58:42 -0700

    used React Bootstrap to center 'About' content and put it in a figure for easy captioning

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import photo from '../../assets/cover-image/about-me-img.jpg';
+import Container from 'react-bootstrap/Container';
+import Figure from 'react-bootstrap/Figure';
+import Row from 'react-bootstrap/Row';
+
 export const About = () => {
     return (
-        <h1>This is our about page</h1>
+        <Container fluid="md">
+            <Row className="justify-content-md-center">
+            <h1>About Me</h1>
+            </Row>
+            <Row className="justify-content-md-center">
+            <Figure>
+                <Figure.Image
+                    width={315}
+                    height={400}
+                    alt="Adam Barron Portrait"
+                    src={photo}
+                />
+                <Figure.Caption>
+                    Adam Barron Bio
+                </Figure.Caption>
+            </Figure>
+            </Row>
+        </Container>
     ) 
 }
